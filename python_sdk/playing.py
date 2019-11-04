ae54e0f106ff53e57af629585cf5a2b4607e4913
Christopher Burgess <christopher.burgess@vortexa.com>
2019-11-04 17:16:50 +0000

    feat: We can now search on geographies

--- python_sdk/playing.py
+++ python_sdk/playing.py
@@ -5,6 +5,11 @@ import requests
 PAGE_SIZE = 500
 REFERENCE_PAGE_SIZE = 2000
 
+GEOGRAPHIES_REFERENCE = "/reference/geographies"
+VESSELS_REFERENCE = "/reference/vessels"
+PRODUCTS_REFERENCE = "/reference/products"
+CHARTERERS_REFERENCE = "/reference/charterers"
+
 API_URL = 'https://api.vortexa.com/v5'
 
 
@@ -21,28 +26,16 @@ class VortexaAPI(object):
         url = f'{API_URL}{resource}/{id}?apikey={self.api_key}'
 
         response = requests.get(url).json()
-
-        logInfo(f'URL: {url}')
-        logInfo(f'Response: {response}')
-
+        # logInfo(f'URL: {url}')
+        # logInfo(f'Response: {response}')
         return response['data']
 
     def search(self, resource, **data):
-        print("searhing")
         url = f'{API_URL}{resource}?apikey={self.api_key}'
-        request = requests.post(url, data=data)
-        response = request.json()
-
-        hand_data = {"term": "portsmouth"}
-
-        print(hand_data)
-        print(data)
-        # response = requests.post("https://api.vortexa.com/v5/reference/geographies?apikey=017e57d2-1302-4ac5-80bc-0a0e3eb1548e", data=hand_data).json()
-
-        logInfo(f'URL: {request.url}')
-        logInfo(f'hedaers: {request.headers}')
-        logInfo(f'Response: {response}')
-
+        # logInfo(f'URL: {request.url}')
+        # logInfo(f'hedaers: {request.headers}')
+        response = requests.post(url, data=data).json()
+        # logInfo(f'Response: {response}')
         return response['data']
 
 
@@ -61,20 +54,19 @@ def default_api():
 class Reference:
     def __init__(self, resource):
         self._resource = resource
-        super().__init__()
 
     def reference(self, id):
         api = default_api()
-        return api.get_reference("/reference" + self._resource, id)
+        return api.get_reference(self._resource, id)
 
 
-class CargoMovements:
-    pass
+class Search:
+    def __init__(self, resource):
+        self._resource = resource
 
-
-#
-#     def __init__(self):
-#         super().__init__("/search/cargo_movements")
+    def search(self, **data):
+        api = default_api()
+        return api.search(self._resource, **data)
 
 
 class Vessels(Reference):
@@ -83,44 +75,65 @@ class Vessels(Reference):
         super().__init__("/vessels")
 
 
-class Geographies(Reference):
+class Geographies(Reference, Search):
 
     def __init__(self):
-        super().__init__("/geographies")
+        Reference.__init__(self, GEOGRAPHIES_REFERENCE)
+        Search.__init__(self, GEOGRAPHIES_REFERENCE)
+
+    def search(self, term):
+        """
+
+        Args:
+            *term*: The geography name we're filtering on
+
+        Returns: List of geographies matching *term*
+
+        Examples:
+
+            >>> [x["name"] for x in Geographies().search(term="portsmouth")]
+            ['Portsmouth [GB]', 'Portsmouth, NH [US]']
+
+        """
+
+        search_params = {"term": term}
+        return super().search(**search_params)
 
 
 class Products(Reference):
 
     def __init__(self):
-        super().__init__("/products")
+        super().__init__("")
 
 
 class VortexaSDK:
 
     def __init__(self):
         default_api()
-        self.cargo_movements: CargoMovements = CargoMovements()
-        self.vessels: Vessels = Vessels()
+        # self.cargo_movements: CargoMovements = CargoMovements()
+        # self.vessels: Vessels = Vessels()
         self.geographies: Geographies = Geographies()
-        self.products: Products = Products()
+        # self.products: Products = Products()
 
 
 v = VortexaSDK()
 
+print(Geographies.__mro__)
+
 # assert False
 # print(default_api().reference("/reference/products", "8d752368ce24f1ef748a89da1cd14c0e4e8bba4042295815a63eaf736eda1a70"))
-default_api().search("/reference/geographies", term="portsmouth")
+# default_api().search("/reference/geographies", term="portsmouth")
 #
 # v.cargo_movements.search(origin='Sture [NO]')
 
 for a in [
-    v.vessels.reference(id='6d8a8f0863ca087204dd68e5fc3b6469a879829e6262856e34856aea3ca20509'),
-    v.geographies.reference(id='48fca2ce4042e5e670a26f1ed2c9dbb8788bfd9bf763deb339791c3e818e2926'),
-    v.products.reference(id='8d752368ce24f1ef748a89da1cd14c0e4e8bba4042295815a63eaf736eda1a70'),
-    v.geographies.reference(id='fdlgkalkh')
-]:
-    print(a)
+    # v.vessels.reference(id='6d8a8f0863ca087204dd68e5fc3b6469a879829e6262856e34856aea3ca20509'),
+    # v.geographies.reference(id='48fca2ce4042e5e670a26f1ed2c9dbb8788bfd9bf763deb339791c3e818e2926'),
+    # v.products.reference(id='8d752368ce24f1ef748a89da1cd14c0e4e8bba4042295815a63eaf736eda1a70'),
+    # v.geographies.reference(id='cfb8c4ef76585c3a37792b643791a0f4ff6d5656d5508927d8017319e21f2fca'),
+    v.geographies.search(term="portsmouth"),  # [0]['name']
+    [x["name"] for x in v.geographies.search(term="portsmouth")],  # [0]['name']
 
-# print(v.cargo_movements.search(id='123234'))
-# print(v.cargo_movements.search(id='123234'))
-# v.vessels.search(id='123234')
+    # v.geographies.search(termo="portsmouth")  # [0]['name']
+]:
+    print(f'{len(a)} {a}')
