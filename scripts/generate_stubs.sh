ff6748ececb7d428b50a6840d104fd3c15699ecb
dstarkey23 <david.starkey@vortexa.com>
2019-11-15 13:43:48 +0000

    test: generate product stubs

--- scripts/generate_stubs.sh
+++ scripts/generate_stubs.sh
@@ -8,3 +8,7 @@ CURL --silent --header "Content-Type: application/json" -X POST \
 CURL --silent --header "Content-Type: application/json" -X POST \
     --data '{"size":2}' "https://api.vortexa.com/v5/reference/vessels?apikey=${VORTEXA_API_KEY}" | jq ".data" \
     > ../tests/api/examples/vessels.json
+
+CURL --silent --header "Content-Type: application/json" -X POST \
+    --data '{"size":2}' "https://api.vortexa.com/v5/reference/products?apikey=${VORTEXA_API_KEY}" | jq ".data" \
+    > ../tests/api/examples/products.json
