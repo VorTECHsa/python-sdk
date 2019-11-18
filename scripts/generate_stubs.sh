8eb78843057a28b6ff780da11ce8849814b224de
Christopher Burgess <christopher.burgess@vortexa.com>
2019-11-18 12:22:14 +0000

    test: Use original vessers.json

--- scripts/generate_stubs.sh
+++ scripts/generate_stubs.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env bash
 
-
-CURL --silent --header "Content-Type: application/json" -X POST \
-    --data '{"size":2}' "https://api.vortexa.com/v5/reference/charterers?apikey=${VORTEXA_API_KEY}" | jq ".data" \
-    > ../tests/api/examples/charterers.json
-
-CURL --silent --header "Content-Type: application/json" -X POST \
-    --data '{"size":2}' "https://api.vortexa.com/v5/reference/vessels?apikey=${VORTEXA_API_KEY}" | jq ".data" \
-    > ../tests/api/examples/vessels.json
-
-CURL --silent --header "Content-Type: application/json" -X POST \
-    --data '{"size":2}' "https://api.vortexa.com/v5/reference/products?apikey=${VORTEXA_API_KEY}" | jq ".data" \
-    > ../tests/api/examples/products.json
+#CURL --silent --header "Content-Type: application/json" -X POST \
+#    --data '{"size":2}' "https://api.vortexa.com/v5/reference/charterers?apikey=${VORTEXA_API_KEY}" | jq ".data" \
+#    > ../tests/api/examples/charterers.json
+#
+#CURL --silent --header "Content-Type: application/json" -X POST \
+#    --data '{"size":2}' "https://api.vortexa.com/v5/reference/vessels?apikey=${VORTEXA_API_KEY}" | jq ".data" \
+#    > ../tests/api/examples/vessels.json
+#
+#CURL --silent --header "Content-Type: application/json" -X POST \
+#    --data '{"size":2}' "https://api.vortexa.com/v5/reference/products?apikey=${VORTEXA_API_KEY}" | jq ".data" \
+#    > ../tests/api/examples/products.json
