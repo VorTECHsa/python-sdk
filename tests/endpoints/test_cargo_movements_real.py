130bf0e1c3d05e0e2e9c1505daaf42cae0b32549
Christopher Burgess <christopher.burgess@vortexa.com>
2020-03-09 17:32:07 +0000

    feat: Add timeseries activity min and max time span

--- tests/endpoints/test_cargo_movements_real.py
+++ tests/endpoints/test_cargo_movements_real.py
@@ -81,6 +81,22 @@ class TestCargoMovementsReal(TestCaseUsingRealAPI):
 
         assert len(df) == 2
 
+    def test_search_filters_on_timeseries_max_activity(self):
+        df = (
+            CargoMovements()
+                .search(
+                filter_activity="storing_state",
+                filter_time_min=datetime(2019, 8, 29),
+                filter_time_max=datetime(2019, 8, 29, 0, 10),
+                timeseries_activity_time_span_min=1000 * 60 * 60 * 24 * 14,
+                timeseries_activity_time_span_max=1000 * 60 * 60 * 24 * 60
+            )
+                .to_df()
+                .head(2)
+        )
+
+        assert len(df) == 2
+
     def test_search_single_filter_owner_name(self):
         df = (
             CargoMovements()
