5b5418d3f2f9d1877872804ea6946e535bcfa782
Christopher Burgess <christopher.burgess@vortexa.com>
2020-03-25 10:23:15 +0000

    chore: New tag 0.14.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="vortexasdk",
-    version="0.14.0",
+    version="0.14.1",
     author="Vortexa Developers",
     author_email="developers@vortexa.com",
     description="Vortexa SDK",
