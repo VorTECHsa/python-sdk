938985fbe8ad853b732bdb28c5721b55a607e349
asfakianakis <axilleas.sfak@gmail.com>
2020-04-16 23:50:47 +0100

    feat: Move vessel_status test to existing class

--- tests/endpoints/test_vessel_movements_real.py
+++ tests/endpoints/test_vessel_movements_real.py
@@ -20,6 +20,16 @@ class TestVesselMovementsReal(TestCaseUsingRealAPI):
 
         assert len(v) > 10
 
+    def test_search_vessel_status(self):
+        v = VesselMovements().search(
+            filter_time_min=datetime(2019, 10, 1),
+            filter_time_max=datetime(2019, 10, 15),
+            filter_vessel_classes=["vlcc"],
+            filter_vessel_status="vessel_status_ballast",
+        )
+
+        assert len(v) > 50
+
     def test_to_df_all_columns(self):
         rotterdam = [
             g.id
