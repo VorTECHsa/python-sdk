f8bc64ec2e45e177b405e653b609d6545eb3b5be
asfakianakis <axilleas.sfak@gmail.com>
2020-04-27 23:07:20 +0100

    feat: add new functionality in utils to handle exlusion dict input

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,10 @@
 from unittest import TestCase
 
-from vortexasdk.utils import convert_to_list, convert_values_to_list
+from vortexasdk.utils import (
+    convert_to_list,
+    convert_values_to_list,
+    convert_values_to_list_abstract,
+)
 
 
 class TestUtils(TestCase):
@@ -17,3 +21,10 @@ class TestUtils(TestCase):
         expected = {1: [], 2: ["b"], 3: ["c"]}
 
         assert convert_values_to_list(d) == expected
+
+    def test_convert_values_to_list_abstract(self):
+        d = {"a": "v1", "b": "v2", "c": ["v3"]}
+
+        expected = {"a": ["v1"], "b": ["v2"], "c": ["v3"]}
+
+        assert convert_values_to_list_abstract(d) == expected
