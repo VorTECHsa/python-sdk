1dfa1cd84dcef98344a31de5259c2ee551b6b0cc
Seán Barry <se@nbarry.co.uk>
2020-05-06 19:10:44 +0100

    chore: add more enums

--- vortexasdk/endpoints/attributes.py
+++ vortexasdk/endpoints/attributes.py
@@ -4,6 +4,7 @@ from vortexasdk.endpoints.endpoints import ATTRIBUTES_REFERENCE
 from vortexasdk.endpoints.attributes_result import AttributeResult
 from vortexasdk.operations import Reference, Search
 from vortexasdk.utils import convert_to_list
+from vortexasdk.enums import AttributesTypesEnum
 
 
 class Attributes(Reference, Search):
@@ -54,7 +55,7 @@ class Attributes(Reference, Search):
     # noinspection PyShadowingBuiltins
     def search(
         self,
-        type: str = None,
+        type: AttributesTypesEnum = None,
         term: Union[str, List[str]] = None,
         ids: Union[str, List[str]] = None,
     ) -> AttributeResult:
--- vortexasdk/enums.py
+++ vortexasdk/enums.py
@@ -2,11 +2,31 @@ from enum import Enum
 
 
 class VesselAttributesEnum(str, Enum):
+    """
+    These are possible values that the `tag` key of a Vessel Attribute can be
+
+    """
+
     FlagTag = "vessel_flag_tag"
     ScrubberTag = "vessel_scrubber_tag"
 
 
 class ScrubbersFittedEnum(str, Enum):
+    """
+    These are the 3 values that the API expects for this filter
+
+    """
+
     Off = ("off",)
     Included = ("inc",)
     Excluded = "exc"
+
+
+class AttributesTypesEnum(str, Enum):
+    """
+    These are possible values that the `type` arg of the Attribute search can be
+
+    """
+
+    IceClass = ("ice_class",)
+    Propulsion = ("propulsion",)
