5d3555a4956adec3c4db33617633aafb873eae5e
asfakianakis <axilleas.sfak@gmail.com>
2020-05-27 17:38:35 +0100

    feat: add test for pkg version

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,8 +1,10 @@
 from unittest import TestCase
+from distutils.version import StrictVersion
 
 from vortexasdk.utils import (
     convert_to_list,
     convert_values_to_list,
+    get_latest_package_version,
 )
 
 
@@ -20,3 +22,10 @@ class TestUtils(TestCase):
         expected = {1: [], 2: ["b"], 3: ["c"]}
 
         assert convert_values_to_list(d) == expected
+
+    def test_get_latest_package_version(self):
+        pd_old_version = "0.25.3"
+
+        lv, _ = get_latest_package_version("pandas")
+
+        assert lv > StrictVersion(pd_old_version)
