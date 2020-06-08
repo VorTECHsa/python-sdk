209cee46205cfcd254b50e71d721c02f8afdb69d
Christopher Burgess <christopher.burgess@vortexa.com>
2020-06-08 16:24:01 +0000

    Deployed 75ca13d with MkDocs version: 1.0.4

--- scripts/deploy_docs.sh
+++ scripts/deploy_docs.sh
@@ -2,4 +2,7 @@
 
 mkdir -p ./_build/pydocmd/examples
 python docs/autogen.py
-pydocmd gh-deploy
+pydocmd serve & sleep 5
+cp mkdocs.yml mkdocs.yml.backup
+echo "google_analytics: ['UA-153895438-1', 'vortechsa.github.io']" >> mkdocs.yml.backup
+pydocmd gh-deploy -f /Users/kit/Development/git_repositories/python-sdk/mkdocs.yml.backup
