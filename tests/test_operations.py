fdd07aad1300a2b02cc808f6265bf5e1248a58f9
Christopher Burgess <christopher.burgess@vortexa.com>
2020-06-29 19:44:45 +0100

    chore: Apply black

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -5,27 +5,33 @@ endpoints_and_searchterms = [
     (Products(), "Gasoil"),
     (Geographies(), "China"),
     (Corporations(), "Oil"),
-    (Vessels(), "Ocean")
+    (Vessels(), "Ocean"),
 ]
 
 
 class TestSearchReal(TestCaseUsingRealAPI):
-
-    def test_search_exact_match_yields_fewer_results_than_non_exact_match(self):
+    def test_search_exact_match_yields_fewer_results_than_non_exact_match(
+        self,
+    ):
 
         for endpoint, term in endpoints_and_searchterms:
-            result_loose_match = endpoint.search(term=term, exact_term_match=False)
-            result_exact_match = endpoint.search(term=term, exact_term_match=True)
+            result_loose_match = endpoint.search(
+                term=term, exact_term_match=False
+            )
+            result_exact_match = endpoint.search(
+                term=term, exact_term_match=True
+            )
 
             assert len(result_exact_match) < len(result_loose_match)
 
     def test_search_exact_match_yields_exact_matches_only(self):
 
         for endpoint, term in endpoints_and_searchterms:
-            result_exact_match = endpoint.search(term=term, exact_term_match=True)
+            result_exact_match = endpoint.search(
+                term=term, exact_term_match=True
+            )
 
-            actual_names = {e['name'] for e in result_exact_match}
+            actual_names = {e["name"] for e in result_exact_match}
 
             # result must be the exact term, or must contain no results if there's no reference objects with that names
             assert actual_names == {term} or actual_names == set()
-
