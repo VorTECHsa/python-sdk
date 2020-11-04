47de55816ee9742d5aa0c442b298054e4b9056a1
dufia <moskalkonrad@gmail.com>
2020-11-04 07:24:09 +0000

    chore: wip

--- tests/endpoints/test_ton_miles_breakdown_real.py
+++ tests/endpoints/test_ton_miles_breakdown_real.py
@@ -17,55 +17,55 @@ class TestTonMilesBreakdownReal(TestCaseUsingRealAPI):
 
         assert len(result) == 1
 
-    # def test_search_returns_all_days(self):
-    #     start = datetime(2019, 11, 1)
-    #     end = datetime(2019, 11, 10)
+    def test_search_returns_all_days(self):
+        start = datetime(2019, 11, 1)
+        end = datetime(2019, 11, 10)
 
-    #     result = TonMilesBreakdown().search(
-    #         filter_activity="loading_state",
-    #         filter_time_min=start,
-    #         filter_time_max=end,
-    #     )
+        result = TonMilesBreakdown().search(
+            filter_activity="loading_state",
+            filter_time_min=start,
+            filter_time_max=end,
+        )
 
-    #     n_days = (end - start).days + 1
+        n_days = (end - start).days + 1
 
-    #     assert n_days == len(result)
+        assert n_days == len(result)
 
-    # def test_to_df(self):
-    #     start = datetime(2019, 11, 1)
-    #     end = datetime(2019, 11, 10)
+    def test_to_df(self):
+        start = datetime(2019, 11, 1)
+        end = datetime(2019, 11, 10)
 
-    #     df = (
-    #         TonMilesBreakdown()
-    #         .search(
-    #             filter_activity="loading_state",
-    #             filter_time_min=start,
-    #             filter_time_max=end,
-    #         )
-    #         .to_df()
-    #     )
+        df = (
+            TonMilesBreakdown()
+            .search(
+                filter_activity="loading_state",
+                filter_time_min=start,
+                filter_time_max=end,
+            )
+            .to_df()
+        )
 
-    #     print(to_markdown(df.head()))
+        print(to_markdown(df.head()))
 
-    #     n_days = (end - start).days + 1
+        n_days = (end - start).days + 1
 
-    #     assert len(df) == n_days
-    #     assert list(df.columns) == ["key", "value", "count"]
+        assert len(df) == n_days
+        assert list(df.columns) == ["key", "value", "count"]
 
-    # def test_to_list(self):
-    #     start = datetime(2019, 11, 1)
-    #     end = datetime(2019, 11, 10)
+    def test_to_list(self):
+        start = datetime(2019, 11, 1)
+        end = datetime(2019, 11, 10)
 
-    #     time_series_list = (
-    #         TonMilesBreakdown()
-    #         .search(
-    #             filter_activity="loading_state",
-    #             filter_time_min=start,
-    #             filter_time_max=end,
-    #         )
-    #         .to_list()
-    #     )
+        time_series_list = (
+            TonMilesBreakdown()
+            .search(
+                filter_activity="loading_state",
+                filter_time_min=start,
+                filter_time_max=end,
+            )
+            .to_list()
+        )
 
-    #     n_days = (end - start).days + 1
+        n_days = (end - start).days + 1
 
-    #     assert len(time_series_list) == n_days
+        assert len(time_series_list) == n_days
--- vortexasdk/endpoints/ton_miles_breakdown.py
+++ vortexasdk/endpoints/ton_miles_breakdown.py
@@ -11,7 +11,12 @@ from vortexasdk.api.shared_types import to_ISODate
 
 
 class TonMilesBreakdown(Search):
-    """Ton Miles Breakdown Endpoint."""
+    """
+    Ton Miles Breakdown Endpoint. Used to retreive the ton miles data as a time series. The aggregation is done
+    on the Vessel Movements data.
+
+    A detailed explanation of the endpoint can be found [here](https://docs.vortexa.com/reference/ton-miles-breakdown).
+    """
 
     def __init__(self):
         Search.__init__(self, TON_MILES_BREAKDOWN)
@@ -49,7 +54,92 @@ class TonMilesBreakdown(Search):
         exclude_vessel_propulsion: Union[ID, List[ID]] = None,
     ) -> TimeSeriesResult:
         """
-        comment
+        Find VesselMovements matching the given search parameters.
+
+        # Arguments
+            breakdown_frequency: Must be one of: 'day', 'week', 'doe_week', 'month', 'quarter' or 'year'.
+
+            filter_activity: Movement activity on which to base the time filter. Must be one of ['loading_state',
+             'loading_start', 'loading_end', 'identified_for_loading_state', 'unloading_state', 'unloading_start',
+              'unloading_end', 'unloaded_state', 'storing_state', 'storing_start', 'storing_end', 'transiting_state',
+               'any_activity'].
+
+            filter_time_min: The UTC start date of the time filter.
+
+            filter_time_max: The UTC end date of the time filter.
+
+            unit: Unit of measurement. Enter 'b' for barrels or 't' for tonnes.
+
+            filter_charterers: A charterer ID, or list of charterer IDs to filter on.
+
+            filter_destinations: A geography ID, or list of geography IDs to filter on.
+
+            filter_origins: A geography ID, or list of geography IDs to filter on.
+
+            filter_owners: An corporation ID, or list of corporation IDs to filter on.
+
+            filter_products: A product ID, or list of product IDs to filter on.
+
+            filter_vessels: A vessel ID, or list of vessel IDs to filter on.
+
+            filter_vessel_classes: A vessel class, or list of vessel classes to filter on.
+
+            filter_vessel_status: The vessel status on which to base the filter. Enter 'vessel_status_ballast' for ballast vessels, 'vessel_status_laden_known' for laden vessels with known cargo (i.e. a type of cargo that Vortexa currently tracks) or 'vessel_status_laden_unknown' for laden vessels with unknown cargo (i.e. a type of cargo that Vortexa currently does not track).
+
+            filter_vessel_age_min: A number between 1 and 100 (representing years).
+
+            filter_vessel_age_max: A number between 1 and 100 (representing years).
+
+            filter_vessel_scrubbers: Either inactive 'disabled', or included 'inc' or excluded 'exc'.
+
+            filter_vessel_flags: A geography ID, or list of geography IDs to filter on.
+
+            filter_vessel_ice_class: An attribute ID, or list of attribute IDs to filter on.
+
+            filter_vessel_propulsion: An attribute ID, or list of attribute IDs to filter on.
+
+            exclude_origins: A geography ID, or list of geography IDs to exclude.
+
+            exclude_destinations: A geography ID, or list of geography IDs to exclude.
+
+            exclude_products: A product ID, or list of product IDs to exclude.
+
+            exclude_vessels: A vessel ID, or list of vessel IDs to exclude.
+
+            exclude_vessel_classes: A vessel class, or list of vessel classes to exclude.
+
+            exclude_charterers: A charterer ID, or list of charterer IDs to exclude.
+
+            exclude_owners: An owner ID, or list of owner IDs to exclude.
+
+            exclude_vessel_flags: A geography ID, or list of geography IDs to exclude.
+
+            exclude_vessel_ice_class: An attribute ID, or list of attribute IDs to exclude.
+
+            exclude_vessel_propulsion: An attribute ID, or list of attribute IDs to exclude.
+
+
+        # Returns
+        `TimeSeriesResult`
+
+        # Example
+
+        ```python
+        >>> from vortexasdk import TonMilesBreakdown, Vessels
+        >>> new_wisdom = [g.id for g in Vessels().search("NEW WISDOM").to_list()]
+        >>> search_result = CargoTimeSeries().search(
+        ...    unit='b',
+        ...    timeseries_frequency='day',
+        ...    filter_vessels=new_wisdom,
+        ...    filter_time_min=datetime(2018, 1, 1),
+        ...    filter_time_max=datetime(2018, 12, 31))
+        >>> df = search_result.to_df()
+
+        ```
+
+        PROVIDE AN EXAMPLE RESPONSE HERE
+
+        [Ton Miles Breakdown Further Documentation](https://docs.vortexa.com/reference/ton-miles-breakdown)
         """
 
         exclude_params = {
