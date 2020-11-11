dc3b34420180f41fc60ae272f1033953335d8b91
dufia <moskalkonrad@gmail.com>
2020-11-11 15:02:19 +0000

    update(eia_forecast): fix tests

--- tests/endpoints/test_eia_forecasts_real.py
+++ tests/endpoints/test_eia_forecasts_real.py
@@ -2,6 +2,7 @@ from tests.testcases import TestCaseUsingRealAPI
 from datetime import datetime
 from tests.timer import Timer
 from vortexasdk.endpoints.eia_forecasts import EIAForecasts
+from docs.utils import to_markdown
 
 
 class TestEIAForecastsReal(TestCaseUsingRealAPI):
@@ -11,21 +12,17 @@ class TestEIAForecastsReal(TestCaseUsingRealAPI):
         filter_time_min = datetime(2020, 1, 20)
         filter_time_max = datetime(2020, 1, 24)
 
-        forecasts = (
-            EIAForecasts()
-            .search(
-                preset=preset,
-                filter_time_min=filter_time_min,
-                filter_time_max=filter_time_max,
-            )
-            .to_list()
+        forecasts = EIAForecasts().search(
+            preset=preset,
+            filter_time_min=filter_time_min,
+            filter_time_max=filter_time_max,
         )
         assert len(forecasts) == 1
+        values = [g.date for g in forecasts.to_list()]
 
-        values = [g.name for g in forecasts]
         assert "2020-01-24T00:00:00.000Z" in values
 
-        print(to_markdown(forecasts))
+        print(to_markdown(forecasts.to_df()))
 
     def test_search_preset_gasoline_exports(self):
 
@@ -33,22 +30,18 @@ class TestEIAForecastsReal(TestCaseUsingRealAPI):
         filter_time_min = datetime(2020, 3, 20)
         filter_time_max = datetime(2020, 4, 20)
 
-        forecasts = (
-            EIAForecasts()
-            .search(
-                preset=preset,
-                filter_time_min=filter_time_min,
-                filter_time_max=filter_time_max,
-            )
-            .to_list()
+        forecasts = EIAForecasts().search(
+            preset=preset,
+            filter_time_min=filter_time_min,
+            filter_time_max=filter_time_max,
         )
         assert len(forecasts) == 5
 
-        values = [g.name for g in forecasts]
+        values = [g.date for g in forecasts.to_list()]
         assert "2020-04-17T00:00:00.000Z" in values
         assert "2020-04-10T00:00:00.000Z" in values
         assert "2020-04-03T00:00:00.000Z" in values
         assert "2020-03-27T00:00:00.000Z" in values
         assert "2020-03-20T00:00:00.000Z" in values
 
-        print(to_markdown(forecasts))
+        print(to_markdown(forecasts.to_df()))
