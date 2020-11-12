9fcf73e1436e6d106b3834973a8ff6fee2d2713d
Christopher Burgess <christopher.burgess@vortexa.com>
2020-11-12 11:14:57 +0000

    ci: Check black in ci, this should fail

--- setup.py
+++ setup.py
@@ -34,6 +34,7 @@ setuptools.setup(
     ],
     extras_require={
         "tests": [
+            "black==20.8b1",
             "mkdocs==1.0.4",
             "mypy==0.770",
             "pytest==5.2.4",
