eb3835e4c331323ce2567b26fbb23483d951de35
dufia <moskalkonrad@gmail.com>
2020-11-17 11:50:52 +0100

    Deployed a59cbe4 with MkDocs version: 1.0.4

--- scripts/deploy_docs.sh
+++ scripts/deploy_docs.sh
@@ -9,18 +9,3 @@ python docs/autogen.py
 # when pydocmd stops 'serving'.
 # To get around this we:
 
-
-#   1. serve the docs locally
-
-# Kill the any process running on port 8000 if it exists, this is the port that pydocmd serves on.
-process_on_our_port=$(sudo lsof -t -i:8000) && sudo kill $process_on_our_port
-pydocmd serve & sleep 5
-
-#   2. backup the generated mkdocs.yml file
-cp mkdocs.yml mkdocs.yml.backup
-
-#   3. append to the backed up file
-echo "google_analytics: ['UA-153895438-1', 'vortechsa.github.io']" >> mkdocs.yml.backup
-
-#   4. deploy to github pages using our backup file
-pydocmd gh-deploy -f ./mkdocs.yml.backup
