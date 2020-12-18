b79c40dc9aaddb9dc27b008a22c38f89f0836bdb
Pawel <pawel.pietruszka99@gmail.com>
2020-12-18 10:18:35 +0000

    chore: trailing whitespace

--- vortexasdk/endpoints/ton_miles_breakdown.py
+++ vortexasdk/endpoints/ton_miles_breakdown.py
@@ -143,7 +143,7 @@ class TonMilesBreakdown(Search):
 
         ```
 
-        returns 
+        returns
 
         |      |key                      |value       |count |
         |-----:|:------------------------|:-----------|-----:|
