b97b5d7da785d8443e0a8674cb6e2cc9f12c8c24
Kit Burgess <kitburgess>
2020-12-30 17:08:07 +0000

    chore: Remove reference to deploy_docs.sh

--- scripts/export_package.sh
+++ scripts/export_package.sh
@@ -26,6 +26,3 @@ rm -rf dist
 pip install wheel
 python3 setup.py sdist bdist_wheel
 python3 -m twine upload -u $TWINE_USERNAME -p $TWINE_PASSWORD dist/*
-
-# Deploy Docs
-./docs/scripts/deploy_docs.sh
