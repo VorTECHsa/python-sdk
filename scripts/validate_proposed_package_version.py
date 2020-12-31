f5cd04d0a9328cd8e6ad7337839295ff6d2ad6c0
Kit Burgess <kitburgess>
2020-12-31 14:02:10 +0000

    chore: Bump version and better error messages

--- scripts/validate_proposed_package_version.py
+++ scripts/validate_proposed_package_version.py
@@ -5,24 +5,28 @@ from vortexasdk.version_utils import get_latest_sdk_version
 
 
 def check_proposed_version_is_allowed(
-    latest_version: str, proposed_version: str
+        latest_version: str, proposed_version: str
 ) -> None:
     """Check that the proposed version is a valid future semver version, or a prerelease."""
 
-    latest = StrictVersion(latest_version)
-    proposed = StrictVersion(proposed_version)
+    print(f"Latest version:  {latest_version}")
+    print(f"Proposed version {proposed_version}")
 
-    if proposed < latest:
+    try:
+        latest = StrictVersion(latest_version)
+        proposed = StrictVersion(proposed_version)
+
+        assert proposed > latest
+    except Exception as e:
         raise Exception(
-            f"The proposed version {proposed} is not allowed. \n"
-            f"This might be because the proposed version already exists, or because it isn't valid semver.\n"
-            f"Valid versions are of the form X.Y.Z. Pre-releases take the form X.Y.Z.a1, X.Y.Z.a2 \n"
-            f"You must change the vortexasdk/__version__.py file to a valid next version, following semver guidelines.\n"
-            f"Refer to https://semver.org for more information on semantic versioning."
+            f"The proposed version {proposed_version} is not allowed. "
+            f"This might be because the proposed version already exists, or because it isn't valid semver. "
+            f"Valid versions are of the form X.Y.Z. Pre-releases take the form X.Y.Z.a1, X.Y.Z.a2 "
+            f"You must change the vortexasdk/__version__.py file to a valid next version, following semver guidelines. "
+            f"Refer to https://semver.org for more information on semantic versioning.", e
         )
-    else:
-        print(f"Latest version:  {latest_version}")
-        print(f"Proposed version {proposed_version} is allowed.")
+
+    print(f"Proposed version {proposed_version} is allowed")
 
 
 if __name__ == "__main__":
