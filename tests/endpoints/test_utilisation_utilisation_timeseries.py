19e1224d832f7f13d8c5aef9902d4a860628b56d
Pawel <pawel.pietruszka99@gmail.com>
2021-06-23 13:17:26 +0100

    chore: utilisation ts docs

--- tests/endpoints/test_utilisation_utilisation_timeseries.py
+++ tests/endpoints/test_utilisation_utilisation_timeseries.py
@@ -1,12 +1,17 @@
 from datetime import datetime
+from vortexasdk.endpoints.geographies import Geographies
+from vortexasdk.endpoints.products import Products
 from vortexasdk.endpoints.utilisation_utilisation_timeseries import UtilisationUtilisationTimeseries
 
 from tests.testcases import TestCaseUsingRealAPI
 
 class TestUtilisationUtilisationTimeSeries(TestCaseUsingRealAPI):
     def test_search_returns_all_days(self):
-        start = datetime(2021, 6, 17)
-        end = datetime(2021, 6, 21)
+        start = datetime(2021, 1, 11)
+        end = datetime(2021, 1, 18)
+
+        rotterdam = [g.id for g in Geographies().search("rotterdam").to_list() if "port" in g.layer]
+        crude = [p.id for p in Products().search("crude").to_list() if "Crude" == p.name]
 
         df = (
             UtilisationUtilisationTimeseries()
@@ -14,9 +19,11 @@ class TestUtilisationUtilisationTimeSeries(TestCaseUsingRealAPI):
                 filter_time_min=start,
                 filter_time_max=end,
                 timeseries_frequency="day",
+                filter_origins=rotterdam,
+                filter_products=crude,
                 timeseries_property="quantity"
             )
             .to_list()
         )
 
-        assert len(df) == 5
+        assert len(df) == 8
