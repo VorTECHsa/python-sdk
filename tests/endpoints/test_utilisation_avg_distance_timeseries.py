86b02fca5a6cc2f29378508a3b74455b6b66384d
Pawel <pawel.pietruszka99@gmail.com>
2021-06-23 13:22:16 +0100

    chore: avg distance docs

--- tests/endpoints/test_utilisation_avg_distance_timeseries.py
+++ tests/endpoints/test_utilisation_avg_distance_timeseries.py
@@ -1,22 +1,30 @@
 from datetime import datetime
+from vortexasdk.endpoints.geographies import Geographies
+from vortexasdk.endpoints.products import Products
 from vortexasdk.endpoints.utilisation_avg_distance_timeseries import UtilisationAvgDistanceTimeseries
 
 from tests.testcases import TestCaseUsingRealAPI
 
 class TestUtilisationAvgDistanceTimeseries(TestCaseUsingRealAPI):
     def test_search_returns_all_days(self):
-        start = datetime(2021, 6, 17)
-        end = datetime(2021, 6, 21)
+        start = datetime(2021, 1, 11)
+        end = datetime(2021, 1, 18)
+
+        rotterdam = [g.id for g in Geographies().search("rotterdam").to_list() if "port" in g.layer]
+        crude = [p.id for p in Products().search("crude").to_list() if "Crude" == p.name]
 
         df = (
             UtilisationAvgDistanceTimeseries()
             .search(
-                timeseries_unit="km",
                 filter_time_min=start,
                 filter_time_max=end,
-                timeseries_frequency="day"
+                timeseries_frequency="day",
+                filter_origins=rotterdam,
+                filter_products=crude,
+                timeseries_property="quantity",
+                timeseries_unit="km"
             )
             .to_df()
         )
 
-        assert len(df) == 5
+        assert len(df) == 8
