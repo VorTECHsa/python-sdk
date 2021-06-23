6963cf1d2053c3e95ef54fbd86eb0d7c712a985e
Pawel <pawel.pietruszka99@gmail.com>
2021-06-23 13:12:04 +0100

    chore: quantity ts docs

--- tests/endpoints/test_utilisation_quantity_timeseries.py
+++ tests/endpoints/test_utilisation_quantity_timeseries.py
@@ -1,12 +1,17 @@
 from datetime import datetime
+from vortexasdk.endpoints.geographies import Geographies
+from vortexasdk.endpoints.products import Products
 from vortexasdk.endpoints.utilisation_quantity_timeseries import UtilisationQuantityTimeseries
 
 from tests.testcases import TestCaseUsingRealAPI
 
 class TestUtilisationQuantityTimeSeries(TestCaseUsingRealAPI):
     def test_search_returns_all_days(self):
-        start = datetime(2021, 6, 17)
-        end = datetime(2021, 6, 21)
+        start = datetime(2021, 1, 11)
+        end = datetime(2021, 1, 18)
+
+        rotterdam = [g.id for g in Geographies().search("rotterdam").to_list() if "port" in g.layer]
+        crude = [p.id for p in Products().search("crude").to_list() if "Crude" == p.name]
 
         df = (
             UtilisationQuantityTimeseries()
@@ -14,10 +19,12 @@ class TestUtilisationQuantityTimeSeries(TestCaseUsingRealAPI):
                 filter_time_min=start,
                 filter_time_max=end,
                 timeseries_frequency="day",
+                filter_origins=rotterdam,
+                filter_products=crude,
                 timeseries_property="quantity",
                 timeseries_unit="t"
             )
             .to_df()
         )
 
-        assert len(df) == 5
+        assert len(df) == 8
--- vortexasdk/endpoints/utilisation_quantity_timeseries.py
+++ vortexasdk/endpoints/utilisation_quantity_timeseries.py
@@ -68,112 +68,128 @@ class UtilisationQuantityTimeseries(Search):
     ) -> TimeSeriesResult:
         """
 
-        Find Aggregate flows between regions, for various products, for various vessels, or various corporations.
-
-        Example questions that can be answered with this endpoint:
-
-        * _How many Crude/Condensate barrels have been imported into China each day over the last year?_
-        * _How many tonnes of Fuel Oil has company X exported from the United States each week over the last 2 years?_
-        * _How have long-term Medium-Sour floating storage levels changed over time?_
+        Sum of cargoes quantity for each day. For frequencies other than ‘day’, the values returned are
+        calculated by summing each daily cargo quantity bucket and returning the total.
 
         # Arguments
-            filter_activity: Cargo movement activity on which to base the time filter. The endpoint only includes cargo
-            movements matching that match this filter in the aggregations. Must be one of ['loading_state',
-             'loading_start', 'loading_end', 'identified_for_loading_state', 'unloading_state', 'unloading_start',
-              'unloading_end', 'storing_state', 'storing_start', 'storing_end', 'transiting_state'].
+
+            timeseries_unit: A numeric metric to be calculated for each time bucket. Must be one of ['b', 'bpd', 't',
+            'tpd', 'c', 'cpd'], corresponding to barrels, barrels per day, metric tonnes, metric tonnes per day,
+            cargo movement count, cargo movement count per day, respectively.
+        
+            timeseries_frequency (string): Frequency denoting the granularity of the time series. Must be one
+            of the following: ['day', 'week', 'doe_week', 'month', 'quarter', 'year'].
+
+            timeseries_property (string): Property on the vessel movement used to build the value of the
+            aggregation. By default it is “quantity”. Must be one of the following: ['quantity’, ‘vessel_class’,
+            ‘vessel_flag’, ‘origin_region’, ‘origin_trading_region’, ‘origin_trading_sub_region’, ‘origin_country’,
+            ‘origin_port’, ‘origin_terminal’, ‘destination_region’, ‘destination_trading_region’,
+            ‘destination_trading_sub_region’, ‘destination_country’, ‘destination_port’, ‘destination_terminal’,
+            'product_group', 'product_group_product', 'product_category', 'product_grade'].
 
             filter_time_min: The UTC start date of the time filter.
 
             filter_time_max: The UTC end date of the time filter.
 
-            filter_corporations: A corporation ID, or list of corporation IDs to filter on.
+            filter_products: A product ID, or list of product IDs to filter on.
 
-            filter_destinations: A geography ID, or list of geography IDs to filter on.
-
-            filter_origins: A geography ID, or list of geography IDs to filter on.
+            filter_charterers: A charterer entity ID, or list of product IDs to filter on.
 
             filter_owners: An owner ID, or list of owner IDs to filter on.
 
-            filter_products: A product ID, or list of product IDs to filter on.
+            filter_origins: A geography ID, or list of geography IDs to filter on.
+
+            filter_destinations: A geography ID, or list of geography IDs to filter on.
 
             filter_vessels: A vessel ID, or list of vessel IDs to filter on.
 
-            filter_storage_locations: A geography ID, or list of geography IDs to filter on.
+            filter_vessel_classes: A vessel class, or list of vessel classes to filter on.
 
-            filter_ship_to_ship_locations: A geography ID, or list of geography IDs to filter on.
+            filter_vessel_flags: A vessel flag ID, or list of vessel flag IDs to filter on.
 
-            filter_waypoints: A geography ID, or list of geography IDs to filter on.
+            filter_vessel_ice_class: A vessel ice class ID, or list of vessel ice class IDs to filter on.
 
-            disable_geographic_exclusion_rules: This controls a popular industry term "intra-movements" and determines
-             the filter behaviour for cargo leaving then entering the same geographic area.
+            filter_vessel_propulsion: A vessel propulsion ID, or list of vessel propulsion IDs to filter on.
 
-            timeseries_activity: The cargo movement activity we want to aggregate on. This param defaults to
-            `filter_activity` if left blank. For example, Let's say we want to aggregate the unloading timestamps of
-             all cargo movements that loaded in 2019, then we'd use `filter_time_min` and `filter_time_max` to specify
-             1st Jan 2019 and 31st Dec 2019 respectively, we'd set `filter_activity='loading_state'` and
-             `timeseries_activity='unloading_state'` to filter on loadings but aggregate on unloadings.
-              `filter_activity` Must be one of ['loading_state',
-             'loading_start', 'loading_end', 'identified_for_loading_state', 'unloading_state', 'unloading_start',
-              'unloading_end', 'storing_state', 'storing_start', 'storing_end', 'transiting_state'].
+            filter_vessel_tag: A time bound vessel tag, or list of time bound vessel tags to filter on.
 
-            timeseries_frequency: Frequency denoting the granularity of the time series. Must be one of ['day', 'week',
-             'doe_week', 'month', 'quarter', 'year']
+            filter_vessel_risk_levels: A vessel risk level, or list of vessel risk levels to filter on.
 
-            timeseries_unit: A numeric metric to be calculated for each time bucket. Must be one of ['b', 'bpd', 't',
-             'tpd', 'c', 'cpd'], corresponding to barrels, barrels per day, metric tonnes, metric tonnes per day,
-              cargo movement count, cargo movement count per day, respectively.
+            filter_vessel_scrubbers: Either inactive 'disabled', or included 'inc' or excluded 'exc'.
 
-            timeseries_activity_time_span_min: The minimum amount of time in milliseconds accounted for in a time series
-             activity. Can be used to request long-term floating storage. For example, to only return floating storage
-             movements that occurred for _more_ than 14 days enter
-             `timeseries_activity_time_span_min=1000 * 60 * 60 * 24 * 14` in conjunction with
-             `filter_activity='storing_state'`.
+            filter_vessel_age_min: A number between 1 and 100 (representing years).
 
-            timeseries_activity_time_span_max: The maximum amount of time in milliseconds accounted for in a time series
-             activity. Can be used to request short-term floating storage. For example, to only return floating storage
-             movements that occurred for _less_ than 14 days enter
-             `timeseries_activity_time_span_max=1000 * 60 * 60 * 24 * 14` in conjunction with
-             `filter_activity='storing_state'`.
+            filter_vessel_age_max: A number between 1 and 100 (representing years).
+            
+            filter_vessel_age_min: A number between 0 and 550000.
+
+            filter_vessel_age_max: A number between 0 and 550000.
+
+            filter_activity: Movement activity on which to base the time filter. Must be one of: `'loading_state'`,
+             `'oil_on_water_state'`, `'unloading_state'`, `'ship_to_ship'`, `'storing_state'`, `'transiting_state'`
+
+            filter_vessel_status: The vessel status on which to base the filter. Enter 'vessel_status_ballast' for ballast vessels, 'vessel_status_laden_known' for laden vessels with known cargo (i.e. a type of cargo that Vortexa currently tracks) or 'any_activity' for any other vessels.
+
+            exclude_filter_products: A product ID, or list of product IDs to exclude.
+
+            exclude_filter_charterers: A charterer entity ID, or list of product IDs to exclude.
+
+            exclude_filter_owners: An owner ID, or list of owner IDs to exclude.
+
+            exclude_filter_origins: A geography ID, or list of geography IDs to exclude.
+
+            exclude_filter_destinations: A geography ID, or list of geography IDs to exclude.
+
+            exclude_filter_vessels: A vessel ID, or list of vessel IDs to exclude.
+
+            exclude_filter_vessel_classes: A vessel class, or list of vessel classes to exclude.
+
+            exclude_filter_vessel_ice_class: A vessel ice class ID, or list of vessel ice class IDs to exclude.
+
+            exclude_filter_vessel_propulsion: A vessel propulsion ID, or list of vessel propulsion IDs to exclude.
+
+            exclude_filter_vessel_tag: A time bound vessel tag, or list of time bound vessel tags to exclude.
+
+            exclude_filter_vessel_risk_levels: A vessel risk level, or list of vessel risk levels to exclude.
+
+            crossfilter_charterer_exists: A boolean to include or exclude the records to those that have a charterer.
 
         # Returns
         `TimeSeriesResult`
 
         # Example
-
-        * _What was the monthly average barrels per day of crude loaded from Rotterdam over the last year?_
+        _Ton days supply (ballast) of vessels originating from the Middle East by daily frequency over the
+        last 7 days, by origin_country breakdown._
 
         ```python
-        >>> from vortexasdk import CargoTimeSeries, Geographies, Products
+        >>> from vortexasdk import UtilisationQuantityTimeseries, Geographies, Products
+        >>> from datetime import datetime
         >>> rotterdam = [g.id for g in Geographies().search("rotterdam").to_list() if "port" in g.layer]
         >>> crude = [p.id for p in Products().search("crude").to_list() if "Crude" == p.name]
-        >>> search_result = CargoTimeSeries().search(
-        ...    timeseries_unit='bpd',
-        ...    timeseries_frequency='month',
+        >>> search_result = UtilisationQuantityTimeseries().search(
         ...    filter_origins=rotterdam,
         ...    filter_products=crude,
-        ...    filter_activity='loading_state',
-        ...    filter_time_min=datetime(2018, 1, 1),
-        ...    filter_time_max=datetime(2018, 12, 31))
+        ...    filter_time_min=datetime(2021, 1, 11),
+        ...    filter_time_max=datetime(2021, 1, 18))
+        ...    timeseries_frequency="day",
+        ...    timeseries_property="quantity"
         >>> df = search_result.to_df()
 
         ```
 
         Gives the following:
 
-        |    | key                      |     count |     value |
+        |    | key                      |     value |     count |
         |---:|:-------------------------|----------:|----------:|
-        |  0 | 2018-01-01T00:00:00.000Z | 0.354839  | 458665    |
-        |  1 | 2018-02-01T00:00:00.000Z | 0.75      | 45024     |
-        |  2 | 2018-03-01T00:00:00.000Z | 0.0645161 |  35663.5  |
-        |  3 | 2018-04-01T00:00:00.000Z | 0.878777  |  12345.2  |
-        |  4 | 2018-05-01T00:00:00.000Z | 0.455932  |   9999.32 |
-        |  5 | 2018-06-01T00:00:00.000Z | 0.777667  |  12234.8  |
-        |  6 | 2018-07-01T00:00:00.000Z | 0.555097  | 987666    |
-        |  7 | 2018-08-01T00:00:00.000Z | 0.290323  | 5318008.1 |
-        |  8 | 2018-09-01T00:00:00.000Z | 0.0333333 | 686888.87 |
-        |  9 | 2018-10-01T00:00:00.000Z | 0.354839  | 234344    |
-        | 10 | 2018-11-01T00:00:00.000Z | 0.2345    | 111111    |
-        | 11 | 2018-12-01T00:00:00.000Z | 0.123129  |  34344.5  |
+        |  0 | 2021-01-11T00:00:00.000Z | 24715     | 2         |
+        |  1 | 2021-01-12T00:00:00.000Z | 31203     | 3         |
+        |  2 | 2021-01-13T00:00:00.000Z | 40851     | 4         |
+        |  3 | 2021-01-14T00:00:00.000Z | 68033     | 4         |
+        |  4 | 2021-01-15T00:00:00.000Z | 60213     | 3         |
+        |  5 | 2021-01-16T00:00:00.000Z | 159031    | 4         |
+        |  6 | 2021-01-17T00:00:00.000Z | 159031    | 4         |
+        |  7 | 2021-01-18T00:00:00.000Z | 122201    | 3         |
+
 
 
         """
