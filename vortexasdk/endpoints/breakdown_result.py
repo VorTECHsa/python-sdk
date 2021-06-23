3bba35738742f377f04c4a70f2621de5cf6c0346
Pawel <pawel.pietruszka99@gmail.com>
2021-06-23 08:36:26 +0100

    refactor: breakdown lists

--- vortexasdk/endpoints/breakdown_result.py
+++ vortexasdk/endpoints/breakdown_result.py
@@ -9,45 +9,50 @@ from vortexasdk.result_conversions import create_dataframe, create_list
 
 logger = get_logger(__name__)
 
+def replace_keys(result):
+    """Creates a list of data entries with keys enriched by references"""
+    refs = result["reference"]
+    data = result["data"]
+        
+    return list(map(lambda x: key_from_ref(x, refs), data))
+
 def key_from_ref(datum, refs):
+    """Reads the label from references and replaces the key in the data with that label"""
     key = datum["key"]
     name = refs[key]["label"]
     return {**datum, "key": name}
 
 
 class BreakdownResult(Result):
-    """Container class that holds the result obtained from calling a time series endpoint."""
+    """Container class that holds the result obtained from calling a breakdown endpoint."""
 
     def to_list(self) -> List[BreakdownItem]:
         """Represents time series as a list."""
         # noinspection PyTypeChecker
-        refs = self["reference"]
-        data = self["data"]
 
-        new_list = list(map(lambda x: key_from_ref(x, refs), data))
+        # data enrichment step - keys from `data` (id of entry) are replaced by labels from `reference` 
+        new_list = replace_keys(self)
 
         return create_list(new_list, BreakdownItem)
 
     def to_df(self, columns=None) -> pd.DataFrame:
-        """Represents the timeseries as a dataframe.
+        """Represents the breakdown as a dataframe.
 
-        Returns a `pd.DataFrame`, of time series items with columns:
-         key: The time series key
-         value: The value of the time series for a given key
-         count: The number of records contributing to this time series record.
+        Returns a `pd.DataFrame`, of breakdown items with columns:
+         key: label of the breakdown item
+         value: The value of the breakdown for a given key
+         count: The number of records contributing to this breakdown record.
 
         # Example:
 
-        If we're aggregating Crude exports in tonnes by day, then the `key` column holds the date,
-        the `value` column holds the Crude exports on that day, and the `count` column holds
-        the number of cargo movements contributing towards this day's tonnage.
+        If we're aggregating origin breakdown by vessel count, then the `key` column holds the name of the country,
+        the `value` column holds the number of movements on that day, and the `count` column holds
+        the number of vessels contributing towards this day's movements.
 
         """
-        
-        refs = self["reference"]
-        data = self["data"]
 
-        new_list = list(map(lambda x: key_from_ref(x, refs), data))
+        # data enrichment step - keys from `data` (id of entry) are replaced by labels from `reference`
+        new_list = replace_keys(self)
 
         df = create_dataframe(
             columns=columns,
