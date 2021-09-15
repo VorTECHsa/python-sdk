6b34cdeaaf9ab8a890f94b386ffbb440d7bb24c4
Pawel <pawel.pietruszka99@gmail.com>
2021-09-15 15:31:07 +0100

    chore: stubs

--- scripts/generate_stubs.sh
+++ scripts/generate_stubs.sh
@@ -17,6 +17,6 @@
 
 CURL --header "Content-Type: application/json" -X POST \
     --data '{"filter_time_max":"2017-11-21T23:59:59.999Z","filter_time_min":"2017-11-21T00:00:00.000Z","unit":"b","size":2}' \
-    "https://api.vortexa.com/v5/vessel-movements/search?apikey=${VORTEXA_API_KEY}" \
+    "https://api-chore-effective-controller.public.kube.develop.vortexa.com/v5/vessel-movements/search?apikey=${VORTEXA_API_KEY}" \
     | jq ".data" \
     > ../tests/api/examples/vessel_movements.json
