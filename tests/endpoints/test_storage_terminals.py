f81df8844441ed5aba65506402aabf4984920b70
OllyDS <0lly@protonmail.com>
2021-11-04 11:43:35 +0000

    chore: update storage terminal tests

--- tests/endpoints/test_storage_terminals.py
+++ tests/endpoints/test_storage_terminals.py
@@ -9,21 +9,25 @@ class TestStorageTerminals(TestCaseUsingMockAPI):
     st = StorageTerminalResult(example_storage_terminals)
 
     def test_search(self):
-        asset_tanks = StorageTerminals().search().to_df()
-        assert len(asset_tanks) > 0
+        terminals = StorageTerminals().search().to_df()
+        assert len(terminals) > 0
 
     def test_name_search_term(self):
-        asset_tanks = StorageTerminals().search(name=['Military']).to_df()
-        assert len(asset_tanks) > 0
+        terminals = StorageTerminals().search(name=['Military']).to_df()
+        assert len(terminals) > 0
 
     def test_search_ids(self):
-        asset_tanks = (
+        terminals = (
             StorageTerminals().search(ids=['08bbaf7a67ab30036d73b9604b932352a73905e16b8342b27f02ae34941b7db5']).to_list()
         )
-        names = [a.name for a in asset_tanks]
+        names = [a.name for a in terminals]
         assert 'Military Oil Depot' in names
 
     def test_to_list(self):
         names = [x.name for x in self.st.to_list()]
 
         assert names == ['Waypoints', 'South Pars Kangan Site - Phase 13', 'Military Oil Depot']
+
+    def test_check_columns(self):
+        terminals = StorageTerminals().search().to_df()
+        assert list(terminals.columns) == ['id', 'name', 'lat', 'lon']
