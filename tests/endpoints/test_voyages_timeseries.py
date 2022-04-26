46cc7f588d3811157be4f70a3d86803c6a2fd3da
Pawel <pawel.pietruszka99@gmail.com>
2022-04-26 15:45:05 +0100

    feat: timeseries docs

--- tests/endpoints/test_voyages_timeseries.py
+++ tests/endpoints/test_voyages_timeseries.py
@@ -2,6 +2,7 @@ from datetime import datetime
 from vortexasdk import VoyagesTimeseries
 
 from tests.testcases import TestCaseUsingRealAPI
+from vortexasdk.endpoints.geographies import Geographies
 
 rotterdam = "68faf65af1345067f11dc6723b8da32f00e304a6f33c000118fccd81947deb4e"
 
@@ -21,3 +22,27 @@ class TestVoyagesTimeseries(TestCaseUsingRealAPI):
             .to_df()
         )
         assert len(df) == 5
+
+    def test_from_description(self):
+        start = datetime(2022, 4, 26)
+        end = datetime(2022, 4, 28, 23, 59)
+
+        rotterdam = [g.id for g in Geographies().search("rotterdam").to_list() if "port" in g.layer]
+
+        df = (
+            VoyagesTimeseries()
+            .search(
+                origins=rotterdam,
+                time_min=start,
+                time_max=end,
+                breakdown_frequency="day",
+                breakdown_property="vessel_count",
+                breakdown_split_property="location_country",
+                breakdown_unit_operator="sum",
+            )
+            .to_df()
+        )
+
+        print(df.to_markdown())
+
+        assert len(time_series_list) > 0
