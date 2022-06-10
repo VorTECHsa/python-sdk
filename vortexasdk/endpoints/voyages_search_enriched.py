31f50409c7281a399a010d5ec95eb5e1cae88396
Pawel <pawel.pietruszka99@gmail.com>
2022-06-10 15:01:07 +0100

    chore: all

--- vortexasdk/endpoints/voyages_search_enriched.py
+++ vortexasdk/endpoints/voyages_search_enriched.py
@@ -33,7 +33,7 @@ class VoyagesSearchEnriched(Search):
         order_direction: str = None,
         offset: int = None,
         unit: str = None,
-        columns: Union[str, List[str]] = None,
+        columns: Union[str, List[str]] = "all",
         time_min: datetime = datetime(2022, 1, 1, 0),
         time_max: datetime = datetime(2022, 1, 1, 1),
         voyage_id: Union[ID, List[ID]] = None,
