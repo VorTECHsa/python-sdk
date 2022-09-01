de576fd109c656c83d230dc73a9f00d6c529ada8
Sean Barry <se@nbarry.co.uk>
2022-09-01 14:38:56 +0100

    feat: types & ci improvements

--- setup.py
+++ setup.py
@@ -50,7 +50,8 @@ setuptools.setup(
             "types-requests==2.28.9",
             "types-urllib3==1.26.23",
             "types-tabulate==0.8.11",
-            "types-python-dateutil==2.8.19"
+            "types-python-dateutil==2.8.19",
+            "types-six==1.16.19"
         ],
         "deploy": ["wheel==0.36.2", "twine==3.3.0"],
     },
