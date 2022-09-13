d748f4d8497e3aac1bfb3c4cd30ee309a1794629
Pawel Pietruszka <pawel.pietruszka99@gmail.com>
2022-09-13 13:40:55 +0100

    feat: new typing for latest products

--- vortexasdk/api/voyages.py
+++ vortexasdk/api/voyages.py
@@ -189,4 +189,4 @@ class VoyageEnrichedItem(BaseModel):
             ]
         ]
     ] = None
-    latest_product_details: Optional[List[EntityWithSingleLayer]] = None
+    latest_product_details: Optional[List[List[EntityWithSingleLayer]]] = None
