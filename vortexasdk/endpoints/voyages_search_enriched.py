a236dbc2cfd08f448ef9fb1b597a314651e16da9
Pawel Pietruszka <pawel.pietruszka99@gmail.com>
2022-09-28 16:23:09 +0100

    chore: remove import

--- vortexasdk/endpoints/voyages_search_enriched.py
+++ vortexasdk/endpoints/voyages_search_enriched.py
@@ -5,7 +5,6 @@ Try me out in your browser:
 """
 from datetime import datetime
 from typing import Any, Dict, List, Union
-from typing_extensions import Literal
 
 from vortexasdk.api import ID
 from vortexasdk.api.shared_types import Tag, to_ISODate
