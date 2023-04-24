2caadcffdfdc39979fd97154ede9fd452292d605
Jacob <116007199+jakubbuskiewicz@users.noreply.github.com>
2023-04-24 08:54:07 +0100

    feat: filter_time using now() as value (#431)
    * feat: use datetime.now() as default filter_time values
    
    * chore(version): bump

--- vortexasdk/endpoints/cargo_movements.py
+++ vortexasdk/endpoints/cargo_movements.py
@@ -36,8 +36,8 @@ class CargoMovements(Record, Search):
     def search(
         self,
         filter_activity: str = None,
-        filter_time_min: datetime = datetime(2019, 10, 1, 0),
-        filter_time_max: datetime = datetime(2019, 10, 1, 1),
+        filter_time_min: datetime = datetime.now(),
+        filter_time_max: datetime = datetime.now(),
         cm_unit: str = "b",
         filter_charterers: Union[ID, List[ID]] = None,
         filter_destinations: Union[ID, List[ID]] = None,
