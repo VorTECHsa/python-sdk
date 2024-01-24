26ce4917ad556f24ada01e5afac7661757c91096
Huan Min Gan <17909473+ministrudels@users.noreply.github.com>
2024-01-24 12:53:32 +0000

    chore: rm conversion util

--- vortexasdk/conversions/__init__.py
+++
@@ -1,4 +0,0 @@
-"""Conversions between various entites."""
-from vortexasdk.conversions.corporations import convert_to_corporation_ids
-from vortexasdk.conversions.geographies import convert_to_geography_ids
-from vortexasdk.conversions.products import convert_to_product_ids
