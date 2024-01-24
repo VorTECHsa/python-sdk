0bebd2901040888b0d05339dc62c397b44216ddc
Huan Min Gan <17909473+ministrudels@users.noreply.github.com>
2024-01-24 12:16:57 +0000

    chore: rm vessel conversion

--- vortexasdk/conversions/__init__.py
+++ vortexasdk/conversions/__init__.py
@@ -2,4 +2,3 @@
 from vortexasdk.conversions.corporations import convert_to_corporation_ids
 from vortexasdk.conversions.geographies import convert_to_geography_ids
 from vortexasdk.conversions.products import convert_to_product_ids
-from vortexasdk.conversions.vessels import convert_to_vessel_ids
