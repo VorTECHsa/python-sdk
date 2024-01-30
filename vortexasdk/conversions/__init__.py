ace79d0812159d9f935798cda37ef9b770423199
Huan Min Gan <17909473+ministrudels@users.noreply.github.com>
2024-01-30 10:39:05 +0000

    Remove reference data conversion util (#469)
    * chore: rm vessel conversion
    
    * chore: rm conversion util
    
    * chore: minor version bump

--- vortexasdk/conversions/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Conversions between various entites."""
-from vortexasdk.conversions.corporations import convert_to_corporation_ids
-from vortexasdk.conversions.geographies import convert_to_geography_ids
-from vortexasdk.conversions.products import convert_to_product_ids
-from vortexasdk.conversions.vessels import convert_to_vessel_ids
