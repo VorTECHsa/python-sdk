c4bd686809597412709e91ece343ba3e5c5cb24d
kdalkas <konstantinos.dalkafoukis@vortexa.com>
2025-04-23 14:41:01 +0300

    feat: update schema

--- vortexasdk/api/refinery.py
+++ vortexasdk/api/refinery.py
@@ -1,7 +1,7 @@
 from pydantic import BaseModel
 from typing import List, Optional
 from vortexasdk.api import ID
-from vortexasdk.api.shared_types import ISODate, Node
+from vortexasdk.api.shared_types import Node
 
 
 class RefineryPort(BaseModel):
@@ -24,13 +24,9 @@ class Refinery(Node):
     id: ID
     country_name: Optional[str] = None
     current_capacity_kbd: Optional[int] = None
-    end_date: Optional[ISODate] = None
-    historical_peak_capacity_kbd: Optional[int] = None
-    is_coastal: Optional[bool] = None
     latitude: Optional[float] = None
     longitude: Optional[float] = None
     name: Optional[str] = None
-    notes: Optional[str] = None
     ports: Optional[List[RefineryPort]] = None
     refinery_operator_id: Optional[str] = None
     refinery_operator_name: Optional[str] = None
@@ -38,8 +34,5 @@ class Refinery(Node):
     refinery_owner_name: Optional[str] = None
     refinery_type: Optional[str] = None
     shipping_region_name: Optional[str] = None
-    start_date: Optional[ISODate] = None
     status: Optional[str] = None
-    storage_location_id: Optional[str] = None
-    storage_location_name: Optional[str] = None
     ref_type: Optional[str] = None
