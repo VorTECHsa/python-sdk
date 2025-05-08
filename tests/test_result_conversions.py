8ed0617b8219d08fc5224b44d91d79ea2b970432
Dhruv Segat <dhruv.segat@vortexa.com>
2025-05-08 11:51:32 +0100

    Parameterise tests

--- tests/test_result_conversions.py
+++ tests/test_result_conversions.py
@@ -1,29 +1,18 @@
 from datetime import datetime
 
 import pandas as pd
+import pytest
 
 from vortexasdk.result_conversions import format_datatypes
 
 
-def test_format_datatypes():
+def test_format_datatypes_does_not_convert_columns_without_timestamp_suffix():
     # Create a sample DataFrame with different timestamp formats
     data = {
         "date_col_dont_convert": ["2023-10-01", "2023-10-02", "2023-07-01"],
-        "date_col_timestamp": ["2023-10-01", "2023-10-02", "2023-07-01"],
-        "mixed_iso_format_timestamps": [
-            "2023-10-03 12:00:00",
-            "2024-01-01 01:01:01.000001",
-            "2025-07-01T00:00:00",
-        ],
-        "unix_timestamp": [
-            1696118400000000000,
-            1696204800000000000,
-            1688169600000000000,
-        ],
         "int_values": [1, 2, 3],
     }
     df = pd.DataFrame(data, dtype=str)
-    df["unix_timestamp"] = df["unix_timestamp"].astype(int)
     df["int_values"] = df["int_values"].astype(int)
 
     # Call the function to format datatypes
@@ -34,36 +23,74 @@ def test_format_datatypes():
         formatted_df["date_col_dont_convert"]
     )
     assert not pd.api.types.is_datetime64_any_dtype(formatted_df["int_values"])
-    pd.testing.assert_series_equal(
-        formatted_df["date_col_timestamp"],
-        pd.Series(
-            [
-                datetime(2023, 10, 1),
-                datetime(2023, 10, 2),
-                datetime(2023, 7, 1),
-            ]
+
+
+@pytest.mark.parametrize(
+    "timestamp_series,col_name,expected_result",
+    (
+        pytest.param(
+            pd.Series(["2023-10-01", "2023-10-02", "2023-07-01"], dtype=str),
+            "date_col_timestamp",
+            pd.Series(
+                [
+                    datetime(2023, 10, 1),
+                    datetime(2023, 10, 2),
+                    datetime(2023, 7, 1),
+                ]
+            ),
+            id="dates are parsed",
         ),
-        check_names=False,
-    )
-    pd.testing.assert_series_equal(
-        formatted_df["mixed_iso_format_timestamps"],
-        pd.Series(
-            [
-                datetime(2023, 10, 3, 12),
-                datetime(2024, 1, 1, 1, 1, 1, 1),
-                datetime(2025, 7, 1),
-            ]
+        pytest.param(
+            pd.Series(
+                [
+                    "2023-10-03 12:00:00",
+                    "2024-01-01 01:01:01.000001",
+                    "2025-07-01T00:00:00",
+                ],
+                dtype=str,
+            ),
+            "mixed_iso_format_timestamps",
+            pd.Series(
+                [
+                    datetime(2023, 10, 3, 12),
+                    datetime(2024, 1, 1, 1, 1, 1, 1),
+                    datetime(2025, 7, 1),
+                ]
+            ),
+            id="mixed ISO formats are parsed",
         ),
-        check_names=False,
-    )
-    pd.testing.assert_series_equal(
-        formatted_df["unix_timestamp"],
-        pd.Series(
-            [
-                datetime(2023, 10, 1),
-                datetime(2023, 10, 2),
-                datetime(2023, 7, 1),
-            ]
+        pytest.param(
+            pd.Series(
+                [
+                    1696118400000000000,
+                    1696204800000000000,
+                    1688169600000000000,
+                ],
+            ),
+            "unix_timestamp",
+            pd.Series(
+                [
+                    datetime(2023, 10, 1),
+                    datetime(2023, 10, 2),
+                    datetime(2023, 7, 1),
+                ]
+            ),
+            id="timestamps ints are parsed",
         ),
+    ),
+)
+def test_format_datatypes_converts_columns_with_timestamp_suffix(
+    timestamp_series: pd.Series, col_name: str, expected_result: pd.Series
+):
+    # Create a sample DataFrame with different timestamp formats
+    df = pd.DataFrame({col_name: timestamp_series})
+
+    # Call the function to format datatypes
+    formatted_df = format_datatypes(df)
+
+    # Check if the columns are converted to datetime
+    pd.testing.assert_series_equal(
+        formatted_df[col_name],
+        expected_result,
         check_names=False,
     )
